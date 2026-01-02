914e056b861a8fb73c9b3d92bf58f8e3306b7b3d
kylea <65596846+goldcakes@users.noreply.github.com>
2026-01-02 06:41:53 +0000

    Bootstrap angular navbar created

--- service-manager/src/app/app.ts
+++ service-manager/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HomePage } from "./pages/home-page/home-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
